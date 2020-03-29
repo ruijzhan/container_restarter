747443a98fd7327ab7d270be77b41c0709aa2015
QiuDog <qiudog825@gmail.com>
2020-03-29 14:47:14 +0800

    init commit

---
+++ functions.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"log"
+	"net"
+	"os"
+)
+
+type myDockerCli struct {
+	*client.Client
+}
+
+func (cli *myDockerCli)containerByName(name string) (container *types.Container, ok bool){
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, container := range containers {
+		for _, cName := range container.Names {
+			if name == cName[1:] {  //[1:] removed prefix '/' from container's name
+				return &container, true
+			}
+		}
+	}
+	return nil, false
+}
+
+func getDockerClient(host, version string) (*myDockerCli, error)  {
+
+	f := func(c *client.Client) error{
+
+			if err := client.WithHost(host)(c); err != nil {
+				return err
+			}
+			if err := client.WithVersion(version)(c); err != nil {
+				return err
+			}
+	    	return nil
+	}
+
+	if cli, err := client.NewClientWithOpts(f); err != nil {
+		return nil, err
+	} else {
+		return &myDockerCli{cli}, nil
+	}
+}
+
+func ipChangeFunc(domain string) func() bool {
+	oldIp := ""
+	return func () bool {
+		ips, err := net.LookupIP(domain)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Cannot resolve %s\n", domain)
+			return  false
+		}
+		ip := ips[0].String()
+
+		if oldIp == "" {
+			oldIp = ip
+			return  false
+		}
+
+		if ip != oldIp {
+			log.Printf("IP address changed from %s to %s", oldIp, ip)
+			oldIp = ip
+			return  true
+		}
+		return  false
+	}
+}
+
+func conditionExec(fExe func() error, fCon func()  bool) error {
+	if  con := fCon(); con {
+		if err := fExe(); err != nil {
+			log.Print(err)
+			return err
+		}
+	}
+	return nil
+}
+
+func restartContainer(cName string, cli *myDockerCli) func() error {
+	return func() error {
+		if container, ok := cli.containerByName(cName); ok {
+			if err := cli.ContainerRestart(context.Background(), container.ID, nil); err != nil {
+				return err
+			} else {
+				log.Printf("Container %s restarted", cName)
+			}
+		} else {
+			return  fmt.Errorf("container %s not found", cName)
+		}
+		return nil
+	}
+}
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"time"
+)
+
+var (
+    container = flag.String("c", "", "Name of container to restart")
+    domain  = flag.String("d", "", "Domain name to watch IP change")
+    interval = flag.Duration("i", time.Duration(10*time.Second), "Time interval to check IP change on domain")
+    host = flag.String("h", "", "docker server host")
+    version = flag.String("v", "1.40", "Docker API version")
+)
+
+func main() {
+	flag.Parse()
+	if *container == "" || *domain == "" || *host == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	cli, err := getDockerClient(*host, *version)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    ipChanged := ipChangeFunc(*domain)
+    restartFunc := restartContainer(*container, cli)
+    for {
+		conditionExec(restartFunc, ipChanged)
+		time.Sleep(*interval)
+	}
+
+}
